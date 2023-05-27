c98b8ae37cf81f96ce86674d0f5ea504025f8a85
Icen <105867625+icenspln@users.noreply.github.com>
2023-05-27 16:43:30 +0100

    added skills section

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -1,24 +1,32 @@
 import "./about.css";
-
 import { Header } from "../../components";
-import yacine from "../../assets/yacine.png"
 
 function About() {
   return (
     <>
       <Header header="About" />
       <main id="about">
-        <div className="about about--secondary">
+        <div className="about">
           <div className="about--info">
             <h3>D.Yacine</h3>
             <br />
             <p>
-              I am a <span>Self_taught programmer,</span> Front-end developer & UI UX designer, I have experice in building websites using React JS, Tailwind CSS and a lot more JS libraries and CSS frameworks.
-              I'm also familier with Design software like Figma.
+              I am a Self_taught programmer, <span>Front-end developer</span>, I
+              have experice in building websites using React JS, Tailwind CSS
+              and a lot more JS libraries and CSS frameworks. I'm also familier
+              with Design software like Figma.
             </p>
           </div>
-          <div className="about--image">
-            <img src={yacine} alt="😎" />
+          <div className="about--skills">
+            <p>
+              I've spent the last two years learning the following technologies:
+            </p>
+            <div className="about--skills-icons">
+              <img src="/tech-stack/html.svg" alt="HTML" />
+              <img src="/tech-stack/css.svg" alt="CSS" />
+              <img src="/tech-stack/js.svg" alt="JS" />
+              <img src="/tech-stack/react.svg" alt="REACT" />
+            </div>
           </div>
         </div>
       </main>
