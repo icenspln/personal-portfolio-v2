37f5ccbcc671fd0bee46b84d774ec6b2f119dd71
Icen <105867625+icenspln@users.noreply.github.com>
2023-06-13 14:30:17 +0100

    added third project

--- src/data/web.js
+++ src/data/web.js
@@ -1,11 +1,15 @@
 import bfstore from "../assets/bfstore.png";
 import weather from "../assets/weather.png"
+import notes from "../assets/notes.png"
+
 const web = [
   {
     id: 1,
     imageUrl: bfstore,
     description:
-      "A FullStack Full Responsive Ecommerce Website with checkout built from scratch.",
+      `A FullStack Full Responsive Ecommerce Website with checkout built from scratch.
+      Using React, TailwindCSS
+      `,
     seeMore: "https://github.com/icenspln/Ecommerce",
     hostUrl: "https://bfstore.vercel.app/",
     hosted: true,
@@ -17,6 +21,15 @@ const web = [
       "Weather app built with open weather API.",
     seeMore: "https://github.com/icenspln/weather-app",
     hostUrl: "https://weather-app-icen.vercel.app",
+    hosted: true,
+  },
+  {
+    id: 3,
+    imageUrl: notes,
+    description:
+      "Note taking application using React, typescript and bootstrap",
+    seeMore: "https://github.com/icenspln/note-app-ts",
+    hostUrl: "https://note-app-ts.vercel.app/",
     hosted: true,
   },
 ];
